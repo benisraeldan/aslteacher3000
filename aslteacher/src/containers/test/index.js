28e3f9d52c2fd6c9f41daa27583bc5a64125b911
alongur123 <guralon2804@gmail.com>
2019-02-28 17:29:56 +0200

    test page

--- aslteacher/src/containers/test/index.js
+++ aslteacher/src/containers/test/index.js
@@ -1,14 +1,53 @@
 import React,{ Component } from "react";
 import '../../index.css';
 import { Link } from 'react-router-dom'
+import { videos } from "../../resources/videos/index";
 
-class App extends Component {
+var video;
+
+function stopRec(camera){
+  camera.stop()    
+  setTimeout(() => window.alert("you are wrong"),2000);    
+}
+
+class App extends Component {  
+  constructor(props){
+    super(props);
+    this.state = {current:0}        
+    this.startRec = this.startRec.bind(this);
+    this.captureCamera = this.captureCamera.bind(this);
+  }
+  captureCamera(callback) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(camera) {
+        callback(camera);
+        setTimeout(() =>stopRec(camera),3000);      
+    }).catch(function(error) {
+        alert('Unable to capture your camera. Please check console logs.');
+        console.error(error);
+    });
+    this.setState({current: this.state.current + 1 % 4});    
+    console.log(this.state);
+}
+startRec(){
+  video = document.querySelector('video');
+    this.captureCamera(function(camera) {
+        video.muted = true;
+        video.volume = 0;
+        video.srcObject = camera;
+    });
+}
   render() {
     return (
-      <div className="centered">     
+      <div>     
+      <button id="btn-start-recording" onClick={()=>this.startRec()}>Start Recording</button>
+  <hr/>
+  <video controls autoPlay playsInline></video>
+
       <Link to="/" className="button">Home</Link>
-      <Link to={"/level/" + this.props.match.params.id} className="button">Back to learning</Link>
-      {this.props.match.params.id}
+      <Link to={"/level/" + this.props.match.params.id} className="button">Back to learning</Link>    
+      <div className="button">
+      <h4>Make the sign for: {videos[this.props.match.params.id - 1][this.state.current].key}</h4>      
+      </div>
       </div>
     );
   }
--- aslteacher/src/resources/videos/index.js
+++ aslteacher/src/resources/videos/index.js
@@ -2,6 +2,19 @@ import boots from "../1/boots.mp4";
 import bra from "../1/bra.mp4";
 import bracelet from "../1/bracelet.mp4";
 import shirt from "../1/shirt.mp4";
+import alligator from "../2/alligator.mp4";
+import bull from "../2/bull.mp4";
+import mouse from "../2/mouse.mp4";
+import tiger from "../2/tiger.mp4";
+import cut from "../3/cut-down.mp4";
+import fly from "../3/fly-animal.mp4";
+import sleep from "../3/sleep.mp4";
+import stand from "../3/stand.mp4";
+import bread from "../4/bread.mp4";
+import cereal from "../4/CEREAL.mp4";
+import frenc_fries from "../4/french-fries.mp4";
+import pancake from "../4/PANCAKE.mp4";
+
 
 export const videos = [
     [{
@@ -11,23 +24,47 @@ export const videos = [
         "key": "bra",
         "value": bra
     }, {
-        "key": "boots",
-        "value": boots
-    }, {
-        "key": "bra",
-        "value": bra
-    }],
-    [{
-        "key": "bracelet",
-        "value": bracelet
-    }, {
         "key": "shirt",
         "value": shirt
     }, {
-        "key": "boots",
-        "value": boots
+        "key": "bracelet",
+        "value": bracelet
+    }],
+    [{
+        "key": "alligator",
+        "value": alligator
     }, {
-        "key": "bra",
-        "value": bra
-    }]
+        "key": "bull",
+        "value": bull
+    }, {
+        "key": "tiger",
+        "value": tiger
+    }, {
+        "key": "mouse",
+        "value": mouse
+    }],[{
+        "key": "cut",
+        "value": cut
+    }, {
+        "key": "fly",
+        "value": fly
+    }, {
+        "key": "sleep",
+        "value": sleep
+    }, {
+        "key": "stand",
+        "value": stand
+    }],[{
+        "key": "bread",
+        "value": bread
+    }, {
+        "key": "cereal",
+        "value": cereal
+    }, {
+        "key": "french fries",
+        "value": frenc_fries
+    }, {
+        "key": "pancake",
+        "value": pancake
+    }],
 ];
